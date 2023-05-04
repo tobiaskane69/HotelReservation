25cccb09200325a0e445bffa34b415855f8d22d2
tobiaskane69 <tobiashernandez47@gmail.com>
2023-05-04 09:53:26 +0800

    1

--- HotelReservation/Dashboard.Designer.cs
+++ HotelReservation/Dashboard.Designer.cs
@@ -28,16 +28,22 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Dashboard));
             button_viewroom = new Button();
             label1 = new Label();
             button_logout = new Button();
-            button_roomprices = new Button();
             button_viewcustomers = new Button();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            pictureBox1 = new PictureBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button_viewroom
             // 
-            button_viewroom.Location = new Point(137, 106);
+            button_viewroom.Location = new Point(137, 113);
             button_viewroom.Name = "button_viewroom";
             button_viewroom.Size = new Size(156, 33);
             button_viewroom.TabIndex = 0;
@@ -48,7 +54,7 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Arial Rounded MT Bold", 18F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(114, 21);
+            label1.Location = new Point(111, 18);
             label1.Name = "label1";
             label1.Size = new Size(207, 28);
             label1.TabIndex = 1;
@@ -56,46 +62,68 @@
             // 
             // button_logout
             // 
-            button_logout.Location = new Point(327, 259);
+            button_logout.BackColor = Color.Transparent;
+            button_logout.Location = new Point(345, 3);
             button_logout.Name = "button_logout";
-            button_logout.Size = new Size(89, 30);
+            button_logout.Size = new Size(81, 30);
             button_logout.TabIndex = 2;
             button_logout.Text = "Logout";
-            button_logout.UseVisualStyleBackColor = true;
+            button_logout.UseVisualStyleBackColor = false;
             button_logout.Click += button_logout_Click;
-            // 
-            // button_roomprices
-            // 
-            button_roomprices.Location = new Point(137, 145);
-            button_roomprices.Name = "button_roomprices";
-            button_roomprices.Size = new Size(156, 33);
-            button_roomprices.TabIndex = 3;
-            button_roomprices.Text = "View Room Prices";
-            button_roomprices.UseVisualStyleBackColor = true;
             // 
             // button_viewcustomers
             // 
-            button_viewcustomers.Location = new Point(137, 184);
+            button_viewcustomers.Location = new Point(137, 161);
             button_viewcustomers.Name = "button_viewcustomers";
             button_viewcustomers.Size = new Size(156, 33);
             button_viewcustomers.TabIndex = 4;
             button_viewcustomers.Text = "View Customers";
             button_viewcustomers.UseVisualStyleBackColor = true;
             // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label1);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(429, 62);
+            panel1.TabIndex = 5;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaption;
+            panel2.Controls.Add(button_logout);
+            panel2.Location = new Point(0, 265);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(429, 38);
+            panel2.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(12, 113);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(100, 81);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 7;
+            pictureBox1.TabStop = false;
+            // 
             // Dashboard
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(428, 301);
+            Controls.Add(pictureBox1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
             Controls.Add(button_viewcustomers);
-            Controls.Add(button_roomprices);
-            Controls.Add(button_logout);
-            Controls.Add(label1);
             Controls.Add(button_viewroom);
             Name = "Dashboard";
-            Text = "Dashboard";
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
-            PerformLayout();
         }
 
         #endregion
@@ -103,7 +131,9 @@
         private Button button_viewroom;
         private Label label1;
         private Button button_logout;
-        private Button button_roomprices;
         private Button button_viewcustomers;
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
     }
 }
--- HotelReservation/Form1.Designer.cs
+++ HotelReservation/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             label1 = new Label();
             label2 = new Label();
             label3 = new Label();
@@ -35,15 +36,21 @@
             txt_password = new TextBox();
             button_login = new Button();
             button_exit = new Button();
+            label4 = new Label();
+            pictureBox1 = new PictureBox();
+            panel1 = new Panel();
+            panel2 = new Panel();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Arial", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(43, 93);
+            label1.Font = new Font("Arial", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(43, 100);
             label1.Name = "label1";
-            label1.Size = new Size(73, 24);
+            label1.Size = new Size(68, 22);
             label1.TabIndex = 0;
             label1.Text = "Log in";
             // 
@@ -70,44 +77,89 @@
             // 
             // txt_username
             // 
+            txt_username.BackColor = SystemColors.InactiveCaption;
             txt_username.Location = new Point(45, 155);
             txt_username.Name = "txt_username";
-            txt_username.Size = new Size(233, 23);
+            txt_username.Size = new Size(271, 23);
             txt_username.TabIndex = 3;
             // 
             // txt_password
             // 
+            txt_password.BackColor = SystemColors.InactiveCaption;
             txt_password.Location = new Point(45, 210);
             txt_password.Name = "txt_password";
             txt_password.PasswordChar = '*';
-            txt_password.Size = new Size(236, 23);
+            txt_password.Size = new Size(271, 23);
             txt_password.TabIndex = 4;
             // 
             // button_login
             // 
-            button_login.Location = new Point(211, 258);
+            button_login.BackColor = SystemColors.MenuHighlight;
+            button_login.Location = new Point(241, 258);
             button_login.Name = "button_login";
             button_login.Size = new Size(75, 31);
             button_login.TabIndex = 5;
             button_login.Text = "Login";
-            button_login.UseVisualStyleBackColor = true;
+            button_login.UseVisualStyleBackColor = false;
             button_login.Click += button_login_Click;
             // 
             // button_exit
             // 
-            button_exit.Location = new Point(130, 258);
+            button_exit.BackColor = SystemColors.MenuHighlight;
+            button_exit.Location = new Point(160, 258);
             button_exit.Name = "button_exit";
             button_exit.Size = new Size(75, 32);
             button_exit.TabIndex = 7;
             button_exit.Text = "Exit";
-            button_exit.UseVisualStyleBackColor = true;
+            button_exit.UseVisualStyleBackColor = false;
             button_exit.Click += button_exit_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Arial Rounded MT Bold", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ControlText;
+            label4.Location = new Point(72, 20);
+            label4.Name = "label4";
+            label4.Size = new Size(221, 22);
+            label4.TabIndex = 8;
+            label4.Text = "HOTEL RESERVATION";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(21, 249);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(90, 85);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 9;
+            pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(label4);
+            panel1.Location = new Point(0, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(373, 64);
+            panel1.TabIndex = 10;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ActiveCaption;
+            panel2.Location = new Point(0, 361);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(373, 43);
+            panel2.TabIndex = 11;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(352, 323);
+            ClientSize = new Size(373, 395);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Controls.Add(pictureBox1);
             Controls.Add(button_exit);
             Controls.Add(button_login);
             Controls.Add(txt_password);
@@ -116,7 +168,9 @@
             Controls.Add(label2);
             Controls.Add(label1);
             Name = "Form1";
-            Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -130,5 +184,9 @@
         private TextBox txt_password;
         private Button button_login;
         private Button button_exit;
+        private Label label4;
+        private PictureBox pictureBox1;
+        private Panel panel1;
+        private Panel panel2;
     }
 }
